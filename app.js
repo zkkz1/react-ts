956da2404c4fcc9be48bb0c7c721f9e1af836112
ziduo.yu01 <ziduo.yu01@liulishuo.com>
2021-07-14 15:06:28 +0800

    feat:添加网页

---
+++ app.js
@@ -0,0 +1,4 @@
+// import './app.css';
+const root = document.querySelector('#root');
+// root.classList.add('bigFont');
+root.innerHTML = 'hello3,webpack';
---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+const path = require('path');
+const { resolve } = require('path');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const getCssLoaders = (importLoaders) => [
+  'style-loader',
+  {
+    loader: 'css-loader',
+    options: {
+      modules: false,
+      sourceMap: isDev,
+      importLoaders,
+    },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      ident: 'postcss',
+      plugins: [
+        // 修复一些和 flex 布局相关的 bug
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
+          autoprefixer: {
+            grid: true,
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+        }),
+        require('postcss-normalize'),
+      ],
+      sourceMap: isDev,
+    },
+  },
+];
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: {
+    app: resolve(__dirname, './app.js'),
+  },
+  output: {
+    filename: isDev ? 'js/[name].js' : 'js/[name].[hash:8].js',
+    path: resolve(__dirname, './dist'),
+  },
+  devServer: {
+    port: 9900, // 指定端口，默认是8080
+    //stats: 'errors-only', // 终端仅打印 error
+    clientLogLevel: 'silent', // 日志等级
+    compress: true, // 是否启用 gzip 压缩
+    open: true, // 打开默认浏览器
+    hot: true, // 热更新
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, './public/index.html'),
+      filename: 'index.html',
+      // cache: fale, // 特别重要：防止之后使用v6版本 copy-webpack-plugin 时代码修改一刷新页面为空问题。
+      minify: isDev
+        ? false
+        : {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            useShortDoctype: true,
+          },
+    }),
+    // new CleanWebpackPlugin(),
+  ],
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       use: getCssLoaders(1),
+  //     },
+  //     {
+  //       test: /\.less$/,
+  //       use: [
+  //         ...getCssLoaders(2),
+  //         {
+  //           loader: 'less-loader',
+  //           options: {
+  //             sourceMap: isDev,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       test: /\.scss$/,
+  //       use: [
+  //         ...getCssLoaders(2),
+  //         {
+  //           loader: 'sass-loader',
+  //           options: {
+  //             sourceMap: isDev,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+};
